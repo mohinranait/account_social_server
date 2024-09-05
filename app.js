c51376693e7438498a4c26ecde8b5911fcbdbc0f
Mohin Rana <ebrahimit49@gmail.com>
2024-09-05 23:35:44 +0600

    nothing

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const rateLimit = require("express-rate-limit")
 const createError = require('http-errors');
 const { errorResponse } = require('./src/helpers/responsHandler');
 const http = require('http');
-const { Server } = require("socket.io")
+// const { Server } = require("socket.io")
 
 
 const app = express();
@@ -71,46 +71,46 @@ app.use((err, req, res, next) => {
 
 // Socket server create
 const expressServer = http.createServer(app)
-const io = new Server(expressServer, {
-    cors: {
-        origin: ['http://localhost:3000'],
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
-    }
-})
-let activeUsers = [];
-io.on('connection', (socket) => {
-    console.log("Add new user", socket.id);
+// const io = new Server(expressServer, {
+//     cors: {
+//         origin: ['http://localhost:3000'],
+//         credentials: true,
+//         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+//     }
+// })
+// let activeUsers = [];
+// io.on('connection', (socket) => {
+//     console.log("Add new user", socket.id);
 
 
-    socket.on('addNewUser', (data) => {
-        const userId = data?.userId;
+//     socket.on('addNewUser', (data) => {
+//         const userId = data?.userId;
 
-        if (!activeUsers?.some(x => x.userId === userId)) {
-            activeUsers.push({
-                userId,
-                socketId: socket.id,
-            })
-        }
-        console.log(activeUsers);
-    })
+//         if (!activeUsers?.some(x => x.userId === userId)) {
+//             activeUsers.push({
+//                 userId,
+//                 socketId: socket.id,
+//             })
+//         }
+//         console.log(activeUsers);
+//     })
 
 
-    socket.on('addMessage', (data) => {
-        const { reciverId, message } = data;
-        const user = activeUsers?.find(user => user?.userId == reciverId)
-        console.log("Data", data);
-        if (user) {
-            console.log('Send Emait');
-            socket.to(user?.socketId).emit('messageReceive', message)
-        }
-    })
+//     socket.on('addMessage', (data) => {
+//         const { reciverId, message } = data;
+//         const user = activeUsers?.find(user => user?.userId == reciverId)
+//         console.log("Data", data);
+//         if (user) {
+//             console.log('Send Emait');
+//             socket.to(user?.socketId).emit('messageReceive', message)
+//         }
+//     })
 
-    socket.on('disconnect', () => {
-        activeUsers = activeUsers?.filter(user => user.socketId !== socket.id)
-        console.log("user disconnect");
-    })
-})
+//     socket.on('disconnect', () => {
+//         activeUsers = activeUsers?.filter(user => user.socketId !== socket.id)
+//         console.log("user disconnect");
+//     })
+// })
 
 
 expressServer.listen(serverPort, () => {
